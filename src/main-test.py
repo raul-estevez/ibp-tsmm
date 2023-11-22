b5ec7723917675e601eb067473c4d1c701731c5f
Raúl Estévez Gómez <estevezgomezraul@gmail.com>
2023-11-22 10:02:30 +0100

    Rework decoder as class and add decoder process in main

--- src/main-test.py
+++ src/main-test.py
@@ -1,22 +1,16 @@
 import numpy as np
 from collections import deque
 from dataclasses import dataclass
-
-#import matplotlib.pyplot as plt
-#import sys
-#np.set_printoptions(threshold=sys.maxsize)
+from multiprocessing import Process, Pipe
 
 from demodulator import Demodulator, params_
-
-from multiprocessing import Process, Pipe
-from time import sleep
+from decoder import Decoder
 
 params = params_()
 
+# Necesario para simular la llegada de datos en tiempo real
 buffer_len = params.buffer_len
 sps = params.sps
-ra_len = params.ra_len
-
 path_envelope = "../resources/convined.grc"
 # Leer la envolvente (el input del módulo) 
 envelope = np.fromfile(path_envelope, dtype=np.float32)
@@ -25,23 +19,26 @@ n_pad = int(np.ceil(len(envelope)/buffer_len))
 envelope = np.pad(envelope, (0, n_pad*buffer_len-len(envelope)))
 envelope = np.reshape(envelope, (int(len(envelope)/buffer_len), buffer_len))
 
-# Demodulator pipes, object and process
-demod_pipe, demod_pipe_p = Pipe()
-demodulator = Demodulator()
-demod_p = Process(target=demodulator.demodulator, args=(demod_pipe_p,))#, args=(demod_pipe_p))
-# Start the demodulator process
-demod_p.start()
+
+############# DEMODULATOR SETUP ############# 
+demod_pipe, demod_pipe_p = Pipe() # Both ends of the demod pipe
+demodulator = Demodulator() # Create a demod object
+demod_p = Process(target=demodulator.demodulator, args=(demod_pipe_p,)) # Create the demod process and pass it its pipe 
+demod_p.start() # Start the demodulator process
 
 
+############# DECODER SETUP ############# 
+decod_pipe, decod_pipe_p = Pipe()
+decoder = Decoder()
+decod_p = Process(target=decoder.decoder, args=(decod_pipe_p,))
+decod_p.start()
 
 for _,data in enumerate(envelope):
     demod_pipe.send(data)
     if demod_pipe.poll():
-        print(demod_pipe.recv())
+        print("Bits recividos del demodulador")
+        decod_pipe.send(list(demod_pipe.recv()))      # Le pasamos los bits al decodificador
 
-
-
-
-
-
-
+        station = decod_pipe.recv()             # Bloqueamos hasta que responda
+        if len(station):
+            print("Estación recibida: " + str(station[0]) + " con " + str(station[1]) + " bits de error")
